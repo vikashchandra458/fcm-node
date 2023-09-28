15b3d74ed157b27fb6d21c6574f463da00bef799
vikash <vikashchandra458@gmail.com>
2023-09-28 21:37:18 +0530

    updated

--- server.js
+++ server.js
@@ -1,51 +1,69 @@
 const express = require("express");
-const bodyparser = require("body-parser");
+const bodyParser = require("body-parser");
 const app = express();
-app.use(bodyparser.json());
-const admin = require("./firebase-config");
+const http = require('http');
 const path = require("path");
-
-
-app.use(
-    bodyparser.urlencoded({
-        extended: true,
-    })
-);
-
-http = require('http');
 const cors = require('cors');
+const admin = require("./firebase-config");
+const { Server } = require('socket.io');
 
-app.use(cors()); // Add cors middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
-
-app.get("/", async function (req, res) {
-    res.sendFile(path.join(__dirname + "/form.html"));
-
-})
-
-app.post("/", async function (req, res) {
-
-    var payload = {
-        notification: {
-            title: req?.body?.title || "Account Deposit",
-            body: req?.body?.body || "A deposit to your savings account has just cleared."
-        }
-    };
-    admin.messaging().sendToDevice(req?.body?.token, payload)
-        .then(function (response) {
-            console.log("Successfully sent message:", response.results[0].messageId);
-            if (response.results[0].messageId) {
-                res.sendFile(path.join(__dirname + "/thankyou.html"));
-            } else (
-                res.sendFile(path.join(__dirname + "/failed.html"))
-
-            )
-        })
-        .catch(function (error) {
-            console.log("Error sending message:", error);
-            res.sendFile(path.join(__dirname + "/failed.html"))
-        });
-
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Socket.IO server is running');
 });
 
-app.listen(4000, () => 'Server is running on port 4000');
+// Create a Socket.IO server by passing the HTTP server
+const io = new Server(server);
+
+// Event handler when a client connects
+io.on('connection', (socket) => {
+  console.log('A client connected');
+
+  // Handle custom events from the client
+  socket.on('locationUpdate', (data) => {
+    console.log('Received location update:', data);
+
+    // Broadcast the location update to all connected clients
+    io.emit('locationUpdate', data);
+  });
+
+  // Event handler when a client disconnects
+  socket.on('disconnect', () => {
+    console.log('A client disconnected');
+  });
+});
+
+app.get("/", async function (req, res) {
+  res.sendFile(path.join(__dirname, "form.html"));
+});
+
+app.post("/", async function (req, res) {
+  var payload = {
+    notification: {
+      title: req?.body?.title || "Account Deposit",
+      body: req?.body?.body || "A deposit to your savings account has just cleared."
+    }
+  };
+  admin.messaging().sendToDevice(req?.body?.token, payload)
+    .then(function (response) {
+      console.log("Successfully sent message:", response.results[0].messageId);
+      if (response.results[0].messageId) {
+        res.sendFile(path.join(__dirname, "thankyou.html"));
+      } else {
+        res.sendFile(path.join(__dirname, "failed.html"));
+      }
+    })
+    .catch(function (error) {
+      console.log("Error sending message:", error);
+      res.sendFile(path.join(__dirname, "failed.html"));
+    });
+});
+
+server.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
